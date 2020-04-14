8b16c290f733bd6dfaf31f3700ca1e9eee427e2b
Gerardo <gahernandez02@gmail.com>
2020-04-13 18:08:52 -0700

    Added GameBoardUnit Testing

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,20 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Gerardo Hernandez on 4/13/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class GameBoardTests: XCTestCase {
+
+   
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+
+}
